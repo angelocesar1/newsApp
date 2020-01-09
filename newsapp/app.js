aaf00de08fe3812c9cff58000d601c984e8ae14f
Angelo Dos Santos <angelodossantos@Angelos-MacBook-Pro.local>
2020-01-09 12:29:39 -0600

    Added login page. Able to compare if user exists in database, if user does not exist redirect to login and give error message. If authenticated res.send(Success)

--- newsapp/app.js
+++ newsapp/app.js
@@ -16,6 +16,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const db = pgp(CONNECTION_STRING);
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  db.oneOrNone(
+    "SELECT userid,username,password FROM users WHERE username =$1",
+    [username]
+  ).then(user => {
+    if (user) {
+      // check for users password
+      bcrypt.compare(password, user.password, function(error, result) {
+        if (result) {
+          res.send("SUCCESS!");
+        } else {
+          res.render("login", { message: "Invalid username or password" });
+        }
+      });
+    } else {
+      // if user does not exist
+      res.render("login", { message: "Invalid username or password" });
+    }
+  });
+});
+
 app.post("/register", (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
