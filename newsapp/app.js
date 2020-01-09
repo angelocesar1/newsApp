74f79bc58b4d360264e06be4e72362d0262bd320
Angelo Dos Santos <angelodossantos@Angelos-MacBook-Pro.local>
2020-01-08 19:14:01 -0600

    Installed mustache and created register page. Created navbar using bootstrap.

--- newsapp/app.js
+++ newsapp/app.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const mustacheExpress = require("mustache-express");
+
+// configuring view engine
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
 app.listen(3000, () => {
   console.log("Server is running...");
