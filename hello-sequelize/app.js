2f3a5eb18b6ed14223121667e55b39124dcfb365
Angelo Dos Santos <angelodossantos@Angelos-MacBook-Pro.local>
2020-01-14 19:50:55 -0600

    Updating entries in database using sequelize

--- hello-sequelize/app.js
+++ hello-sequelize/app.js
@@ -1,5 +1,20 @@
 const models = require("./models");
 
+models.Dish.update(
+  {
+    name: "Carrot Cake",
+    price: 8.0
+  },
+  {
+    where: {
+      id: 2
+    }
+  }
+).then(updatedDish => console.log(updatedDish));
+
+/////////////////////////////////////
+// Logging Items from database into the terminal
+
 // models.Dish.findAll().then(dishes => console.log(dishes));
 
 // models.Dish.findByPk(3).then(dish => console.log(dish));
@@ -10,8 +25,8 @@ const models = require("./models");
 //   }
 // }).then(dishes => console.log(dishes));
 
-models.Dish.findOne({
-  where: {
-    name: "Spring Rolls"
-  }
-}).then(dish => console.log(dish));
+// models.Dish.findOne({
+//   where: {
+//     name: "Spring Rolls"
+//   }
+// }).then(dish => console.log(dish));
