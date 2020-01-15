743942e7f7ae1030d200ae98cc5d217815fc01e4
Angelo Dos Santos <angelodossantos@Angelos-MacBook-Pro.local>
2020-01-14 20:00:49 -0600

    Adding Items and Deleting Items from database using sequelize

--- hello-sequelize/app.js
+++ hello-sequelize/app.js
@@ -1,16 +1,41 @@
 const models = require("./models");
 
-models.Dish.update(
-  {
-    name: "Carrot Cake",
-    price: 8.0
-  },
-  {
-    where: {
-      id: 2
-    }
-  }
-).then(updatedDish => console.log(updatedDish));
+/////////////////////////////////////
+// Adding Items to Database
+
+// let dish = models.Dish.build({
+//   name: "Cake",
+//   description: "This cake is delicious!",
+//   price: 10.5
+// });
+
+// dish.save().then(persistedDish => {
+//   console.log(persistedDish);
+// });
+
+/////////////////////////////////////
+// Deleting Items in Database
+
+// models.Dish.destroy({
+//   where: {
+//     id: 3
+//   }
+// }).then(result => console.log(result));
+
+/////////////////////////////////////
+// Updating Items in Database
+
+// models.Dish.update(
+//   {
+//     name: "Carrot Cake",
+//     price: 8.0
+//   },
+//   {
+//     where: {
+//       id: 2
+//     }
+//   }
+// ).then(updatedDish => console.log(updatedDish));
 
 /////////////////////////////////////
 // Logging Items from database into the terminal
